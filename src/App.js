33e688f7b18c9b0d944186eb55375f7023148954
AlekseiZhukov <alex__zhukov@mail.ru>
2022-06-21 18:19:26 +0300

    init project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Packaging from "./components/Packaging/Packaging";
+
+const App = () => {
+  return (
+
+      <div>
+          App
+          <Packaging />
+      </div>
+
+  );
+};
+
+export default App;
